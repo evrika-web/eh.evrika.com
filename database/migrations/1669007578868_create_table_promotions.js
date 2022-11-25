01eb63896622b26e072cf44ae37af27d0ac374b6
neizvel <neizvel55@gmail.com>
2022-11-25 17:55:42 +0600

    nothing changed

--- database/migrations/1669007578868_create_table_promotions.js
+++ database/migrations/1669007578868_create_table_promotions.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "up": "CREATE TABLE promotions (promotion_id INT NOT NULL, UNIQUE KEY promotion_id (promotion_id), doc_number TINYTEXT, type TINYTEXT, start_date DATETIME, end_date DATETIME, percents JSON, products JSON, participate BOOLEAN )",
+    "up": "CREATE TABLE promotions (promotion_id INT NOT NULL AUTO_INCREMENT, UNIQUE KEY promotion_id (promotion_id), doc_number TINYTEXT, type TINYTEXT, start_date DATETIME, end_date DATETIME, percents JSON, products JSON, participate BOOLEAN )",
     "down": "DROP TABLE promotions"
 }
