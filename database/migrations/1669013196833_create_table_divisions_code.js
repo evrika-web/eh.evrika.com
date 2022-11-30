9b61832da005ec55e05214fea7d583f4bd282e23
neizvel <neizvel55@gmail.com>
2022-11-30 16:56:13 +0600

    delete old migrations

--- database/migrations/1669013196833_create_table_divisions_code.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "up": "CREATE TABLE divisions_code (id INT NOT NULL, UNIQUE KEY id (id), code TINYTEXT, site_id TINYTEXT, name TEXT)",
-    "down": "DROP TABLE divisions_code"
-}
--- database/migrations/1669373673647_add_active_to_promotions.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "up": "ALTER TABLE promotions ADD active BOOL;",
-    "down": "ALTER TABLE promotions DROP COLUMN active;"
-}
