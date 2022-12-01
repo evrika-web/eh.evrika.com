52573d3b44ba73b42ac8176cd86e2eb920472890
neizvel <neizvel55@gmail.com>
2022-12-01 18:51:12 +0600

    Пока что убрал запуск функций по времени

--- scheduledJobs/schedule.js
+++ scheduledJobs/schedule.js
@@ -14,26 +14,26 @@ opts = {
 const log = SimpleNodeLogger.createSimpleLogger(opts);
 
 //schedules 
-schedule.scheduleJob({hour: 03, minute: 02}, async () => {
-    log.info(moment().format('HH:mm DD-MM-YYYY'), 'Time to check Promotions From 1C')
-     try{
-       var result = await getPromotions.getPromotionsfrom1C(); 
-       var updateDB = await getPromotions.setPromotion(result);
-       log.info(updateDB);
-     }catch (err) {
-       log.info(err);
-       console.log(err);
-     }
-   });
+// schedule.scheduleJob({hour: 03, minute: 02}, async () => {
+//     log.info(moment().format('HH:mm DD-MM-YYYY'), 'Time to check Promotions From 1C')
+//      try{
+//        var result = await getPromotions.getPromotionsfrom1C(); 
+//        var updateDB = await getPromotions.setPromotion(result);
+//        log.info(updateDB);
+//      }catch (err) {
+//        log.info(err);
+//        console.log(err);
+//      }
+//    });
    
-   schedule.scheduleJob({hour: 18, minute: 26}, async () => {
-     log.info(moment().format('HH:mm DD-MM-YYYY'), ' Time to check end date of promotions')
-      try{
-        var result = await checkDatePromotion.checkDatePromo(); 
-       //  log.info(result);
-       console.log('Schedule check end date: ', result);
-      }catch (err) {
-        log.info('Schedule check end date error: ', err);
-        console.log(err);
-      }
-    });
+//    schedule.scheduleJob({hour: 18, minute: 26}, async () => {
+//      log.info(moment().format('HH:mm DD-MM-YYYY'), ' Time to check end date of promotions')
+//       try{
+//         var result = await checkDatePromotion.checkDatePromo(); 
+//        //  log.info(result);
+//        console.log('Schedule check end date: ', result);
+//       }catch (err) {
+//         log.info('Schedule check end date error: ', err);
+//         console.log(err);
+//       }
+//     });
