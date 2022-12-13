babe568a4640e4f24a56b7764b84b91cc06e3aa6
neizvel <neizvel55@gmail.com>
2022-12-13 18:39:13 +0600

    add query for product exist and get percents

--- database/dbQuerie.js
+++ database/dbQuerie.js
@@ -64,14 +64,43 @@ module.exports = {
             callback(undefined)
         })
     },
-    productExistCascade: async (article, callback) => {
+    productExistCascade: async (articles, callback) => {
         await knex('promotions')
-        .where('active',true)
-        .andWhere('type','cascade')
-        // .whereLike('products', '%'+article+'%')
-        .whereJsonObject('products',  [article] )
-        // .whereJsonPath('products', '$', '=', [article])
+        .where({active: true, type:'cascade' })
+        .whereJsonObject('products',  [articles] )
         .select('doc_number')
+        .then( (response) => {
+            callback(response)
+        })
+        .catch((err) => {
+            console.log(err)
+            callback(undefined)
+        })
+    },
+    // productExistCascade2: async (articles, callback) => {
+    //         let products = []            
+    //         for(var i in articles){
+    //             await knex('promotions')
+    //             .where({active:true, type:'cascade'})
+    //             .whereJsonObject('products',  [articles[i]] )
+    //             .select('promotion_id')
+    //             .then( (response) => {
+    //                 console.log("response ", response)
+    //                 if(Array.isArray(response) && response.length!=0)
+    //                 {products.push(articles[i])}
+    //                 console.log("products ", products)
+    //             })from string
+    //             .catch((err) => {
+    //                 console.log(err)
+    //             })                
+    //         }
+    //         console.log("callback(products) ", products)
+    //         callback(products)
+    //     },
+    percentsCascade: async (doc_number, callback) => {
+        await knex('promotions')
+        .where('doc_number', doc_number)
+        .jsonExtract('percents','$','cascadePercents')
         .then( (response) => {
             callback(response)
         })
