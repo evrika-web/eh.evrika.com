c6d809aa558c54714e30b75159cbfb02a2f79a21
Neizvel <neizvel55@gmail.com>
2025-04-23 10:18:35 +0500

    added KBT warehouse

--- api/ozonMarket/ozonMarketApi.js
+++ api/ozonMarket/ozonMarketApi.js
@@ -13,25 +13,35 @@ const configURL = {
     "Api-Key": process.env.OZON_API_KEY || "",
     "Client-Id": process.env.OZON_API_CLIENT_ID || "",
   },
-  data: {
-    filter: {},
-    last_id: "",
-    limit: 1000,
-  },
   method: "POST",
 };
 const ozonURL = process.env.OZON_API_URL || "https://api-seller.ozon.ru";
 
 async function getAllProduct() {
+  configURL.data = {
+    filter: {},
+    last_id: "",
+    limit: 1000,
+  };
   try {
     const products = await axios(ozonURL + "/v3/product/list", configURL);
-    return products.data.result.items.filter((item) => item.archived === false);
+    return products.data.result.items;
   } catch (error) {
     console.error("Error fetching products from Ozon API:", error.message);
     throw error;
   }
 }
 
+async function getAllProductsInfo(data) {
+  configURL.data = data;
+  try {
+    const products = await axios(ozonURL + "/v3/product/info/list", configURL);
+    return products.data.items;
+  } catch (error) {
+    console.error("Error fetching products from Ozon API:", error.message);
+    throw error;
+  }
+}
 async function updateCostsProduct({ data }) {
   try {
     const products = await axios.post(
@@ -74,8 +84,14 @@ async function getDataFromWebsite({ vendorCode }) {
     );
     if (response.status === 200) {
       var stock = response.data.branches
-        .filter(branch => branch.guid === "4318444d-c0be-11e1-9c78-001e670c9281")
-        .reduce((acc, branch) => acc + ((branch.quality && branch.quality["Новый"]) || 0), 0);
+        .filter(
+          (branch) => branch.guid === "4318444d-c0be-11e1-9c78-001e670c9281"
+        )
+        .reduce(
+          (acc, branch) =>
+            acc + ((branch.quality && branch.quality["Новый"]) || 0),
+          0
+        );
 
       const data = {
         stock: stock,
@@ -94,6 +110,17 @@ async function getDataFromWebsite({ vendorCode }) {
 
 async function updateStockCostOzon() {
   const allProducts = await getAllProduct();
+  const offerIds = allProducts.map((product) => product.offer_id);
+  const allProductsInfo = await getAllProductsInfo({ offer_id: offerIds });
+  const mergedProducts = allProducts.map((product) => {
+    const productInfo = allProductsInfo.find(
+      (info) => info.offer_id === product.offer_id
+    );
+    return { ...product, ...productInfo };
+  });
+  allProducts.length = 0;
+  allProducts.push(...mergedProducts);
+
   for (const product of allProducts) {
     try {
       const websiteData = await getDataFromWebsite({
@@ -115,7 +142,9 @@ async function updateStockCostOzon() {
       currency_code: "KZT",
       offer_id: item.offer_id,
       price: item.websiteData ? item.websiteData.price.toString() || "0" : "0",
-      old_price: item.websiteData ? item.websiteData.oldPrice.toString() || "0" : "0",
+      old_price: item.websiteData
+        ? item.websiteData.oldPrice.toString() || "0"
+        : "0",
       price_strategy_enabled: "DISABLED",
       product_id: item.product_id,
       quant_size: 1,
@@ -123,30 +152,39 @@ async function updateStockCostOzon() {
     };
   });
   const ozonStockData = allProducts.map((item) => {
+    var warehouse_id = 1020005000165063;
+    if (item.is_kgt) {
+      warehouse_id = 1020005000166135;
+    }
     return {
       offer_id: item.offer_id,
       product_id: item.product_id,
       quant_size: 1,
       stock: item.websiteData ? item.websiteData.stock || 0 : 0,
-      warehouse_id: 1020005000165063,
+      warehouse_id: warehouse_id,
     };
   });
-
+  var responseCosts = null;
+  var responseStocks = null;
   try {
-    const costResponse = await updateCostsProduct({
+    responseCosts = await updateCostsProduct({
       data: { prices: ozonPriceData },
     });
   } catch (error) {
     console.error("Error updating costs:", error.message);
   }
   try {
-    const stockResponse = await updateStocksProduct({
+    responseStocks = await updateStocksProduct({
       data: { stocks: ozonStockData },
     });
   } catch (error) {
     console.error("Error updating stocks:", error.message);
   }
-  return { status: 200 };
+  return {
+    status: 200,
+    responseCosts: responseCosts,
+    responseStocks: responseStocks,
+  };
 }
 module.exports = {
   getAllProduct,
